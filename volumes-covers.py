df34615e4057a183a99704825bca9b52fb035aae
Julien Bidoret <julien@accentgrave.net>
2022-04-28 15:57:31 +0200

    Modifs Arthur

--- volumes-covers.py
+++ volumes-covers.py
@@ -24,26 +24,26 @@ publisher_maximum_height = 0
 publisher_y = 0
 publisher_x = 0
 margin = 10
+arthuridx = 0
 
 for row_idx, row in enumerate(reader):
     
-    # fetch data from csv
-    publisher = row[0]
-    title = row[1].replace('&', '&amp;')
-    book_ref = row[5]
-    width = row[7].replace(',','.') # inconsistencies with csv numbers
-    height = row[8].replace(',','.') # inconsistencies with csv numbers
     
-    # init publisher
-    if not row[0] in publishers:            
+    # arthur :
+
+    size = row[0]
+    width = size.split("x")[0].replace(',','.')
+    height = size.split("x")[1].replace(',','.')
+    if arthuridx == 20:            
         print('====')
-        print(publisher)
+        
         # reset values for each publisher
         publisher_y = publisher_y + publisher_maximum_height + margin
         publisher_x = 0
         publisher_maximum_height = 0
         # append publisher to list
-        publishers.append(publisher)
+        arthuridx = 0
+
     
     # start svg <g>
     file.write( "<g id=\"book_{}\">\n".format(book_ref) ) 
@@ -57,20 +57,10 @@ for row_idx, row in enumerate(reader):
     
     # draw rect
     print('----')
-    print(title)
+    # print(title)
     file.write('<rect id="rect_{}" x="{}" y="{}" fill="rgb(0,0,0)" width="{}" height="{}"/>\n'.format(row_idx, publisher_x, publisher_y, px_width, px_height))
 
-    # draw text (one word by line)
-    text = publisher.strip().split(" ") + ["–"] + title.strip().split(" ")
-    if len(text) >= 10:
-        text = text[:10] + ["…"]
-    text_y = publisher_y + 16
-    text_x = publisher_x + 10
-    for t in text:
-        file.write('<text font-family="\'HelveticaNeueLTStd-BdCn\'" transform="matrix(1 0 0 1 {} {})" fill="rgb(255,255,255)" font-size="{}">{}</text>\n'.format(text_x, text_y, fontSize, t))
-        text_y = text_y + fontSize * 1.1
 
-    # end svg <g>
     file.write("</g>\n")
     publisher_x = publisher_x + px_width + margin
     
